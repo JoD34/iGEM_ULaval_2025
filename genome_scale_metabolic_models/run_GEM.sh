7d393a6f8bc6a590cc272b0b452ca9fcdec9fa07
Alison Ouellet <alison@narval3.narval.calcul.quebec>
2025-08-11 15:51:02 -0400

    Modif

--- genome_scale_metabolic_models/run_GEM.sh
+++ genome_scale_metabolic_models/run_GEM.sh
@@ -1,31 +1,17 @@
 #!/bin/bash
 #SBATCH --account=def-stbil30-ab
 #SBATCH --cpus-per-task=10
-#SBATCH --mem=16G
-#SBATCH --time=4:00:00
+#SBATCH --mem 16G
+#SBATCH -t 4:00:00              # time (D-HH:MM)
 #SBATCH --job-name=Genome-Scale-Metabo
-#SBATCH --output=gem-%j.out
-#SBATCH --error=gem-%j.err
+#SBATCH --output=gem%j.out
+#SBATCH --error=gem%j.err
 
-# --- 1. Modules --------------------------------------------------------------
-module load StdEnv/2023 python/3.12   # <- version supportée
-export OMP_NUM_THREADS=1               # coupe le multithreading BLAS
-export OPENBLAS_NUM_THREADS=1
-export MKL_NUM_THREADS=1
-export NUMEXPR_NUM_THREADS=1
-
-# --- 2. Virtualenv -----------------------------------------------------------
-VENV=$SLURM_TMPDIR/env
-python -m venv $VENV           # --no-download n’est plus nécessaire avec venv
-source $VENV/bin/activate
-
-# Si tu as préparé un wheelhouse local, décompresse-le ici
-# tar -xzf $PROJECT/wheels.tar.gz -C $SLURM_TMPDIR/wheels
-
+module load StdEnv/2020 python/3.11
+virtualenv --no-download $SLURM_TMPDIR/ENV
+source $SLURM_TMPDIR/ENV/bin/activate
 pip install --no-index --upgrade pip
-pip install --no-index -r $PROJECT/genome_scale_metabolic_models/requirements.txt \
-            # --find-links $SLURM_TMPDIR/wheels   # si wheelhouse perso
 
-# --- 3. Lancement ------------------------------------------------------------
-cd   $PROJECT/genome_scale_metabolic_models
+pip install --no-index -r requirements.txt
+
 python GEM.py
